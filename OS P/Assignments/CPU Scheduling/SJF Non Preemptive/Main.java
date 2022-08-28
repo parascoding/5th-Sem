39aa9b5e7e739ffbb54fc6083a363c5cb24569f1
parascoding <parascoding@gmail.com>
2022-08-28 19:24:10 +0530

    Added

--- OS P/Assignments/CPU Scheduling/SJF Non Preemptive/Main.java
+++ OS P/Assignments/CPU Scheduling/SJF Non Preemptive/Main.java
@@ -6,15 +6,109 @@ public class Main{
     public static void main(String args[]) throws IOException{
         br = new BufferedReader(new FileReader("input.txt"));
         ot = new PrintWriter(System.out);
+        listOfProcesses = new ArrayList<>();
+        numberOfProcesses = Integer.parseInt(br.readLine().trim());
         
+        System.out.println(numberOfProcesses);
+        getInput();
+
+        sortProcessesBasedOnAT();
         
+        SJF();
+
+        printProcesses();
+
         ot.close();
         br.close();
     }
+    static void SJF(){
+        PriorityQueue<Process> pq = new PriorityQueue<>(
+            new Comparator<Process>(){
+                public int compare(Process p1, Process p2){
+                    if(p1.bt == p2.bt){
+                        if(p1.at > p2.at)
+                            return 1;
+                        return -1;
+                    }
+                    if(p1.bt > p2.bt)
+                        return 1;
+                    return -1;
+                }
+            }
+        );
+
+        double curTime = listOfProcesses.get(0).at;
+
+        int index = 0;
+        while(index < numberOfProcesses &&
+                curTime >= listOfProcesses.get(index).at){
+            pq.add(listOfProcesses.get(index++));
+        }
+
+        while(!pq.isEmpty()){
+            Process process = pq.remove();
+            curTime = Math.max(curTime, process.at);
+            process.ct = curTime + process.bt;
+            process.tat = process.ct - process.at;
+            process.wt = process.tat - process.bt;
+            curTime += process.bt;
+            while(index < numberOfProcesses &&
+                    curTime >= listOfProcesses.get(index).at){
+                pq.add(listOfProcesses.get(index++));
+            }
+        }
+    }
+    static void printProcesses(){
+        for(Process process : listOfProcesses)
+            ot.println(process);
+    }
+    static void sortProcessesBasedOnAT(){
+        Collections.sort(listOfProcesses,
+            new Comparator<Process>(){
+                public int compare(Process p1, Process p2){
+                    if(p1.at == p2.at){
+                        return p1.pid - p2.pid;
+                    }
+                    if(p1.at > p2.at)
+                        return 1;
+                    return -1;
+                }
+            }
+        );
+    }
+    static void getInput(){
+        try{
+
+            for(int i = 0; i < numberOfProcesses; i++){
+                String s[] = br.readLine().trim().split(" ");
+
+                listOfProcesses.add(new Process(
+                    Integer.parseInt(s[0]),
+                    Double.parseDouble(s[1]),
+                    Double.parseDouble(s[2])
+                ));
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+            return;
+        }
+
+    }
+    static List<Process> listOfProcesses;
+    static int numberOfProcesses;
     static class Process{
         int pid;
-        float at, bt, ct, wt;
-
-        Process(int pid, int at, int bt)
+        double at, bt, ct, wt, tat;
+        
+        Process(int pid, double at, double bt){
+            this.pid = pid;
+            this.at = at;
+            this.bt = bt;
+        }
+        public String toString(){
+            return "{PID: "+pid+", AT: "+at+", BT: "+bt+
+                ", CT: "+ct+", TAT: "+tat+", WT: "+wt+"}";
+            
+        }
     }
 }
