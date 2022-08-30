d21884603c929ff3c06fb7e2fb3f895fa1d1ef5b
parascoding <parascoding@gmail.com>
2022-08-30 11:30:49 +0530

    added scoet programming java

--- DCN P/Networking Task 1/Custom/Client.java
+++ DCN P/Networking Task 1/Custom/Client.java
@@ -5,12 +5,65 @@ import java.io.*;
  public class Client{
     public static void main(String args[]){
         Client client = new Client(args[0], Integer.parseInt(args[1]));
+        int count=0;
+        while(true){
+            String input = client.readClient();
+            if(input.equals("stop"))
+                break; 
+            client.writeServer(input);
+
+            String serverInput = client.readServer();
+            client.writeClient(serverInput);
+
+        }
+        client.close();
     }
+    
     static Socket socket;
     static BufferedReader br;
     static BufferedReader serverReader;
     static PrintWriter ot;
-
+    static void close(){
+        try{
+            br.close();
+            ot.close();
+            socket.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    static void writeClient(String s){
+        System.out.println(s);
+    }
+    static String readServer(){
+        String response = "";
+        try{
+            response = serverReader.readLine();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return response;
+    }
+    static void writeServer(String s){
+        try{
+            ot.println(s);
+            ot.flush();
+        } catch(Exception e){
+        
+            e.printStackTrace();
+        }
+    }
+    static String readClient(){
+        String s = "";
+        System.out.println("Enter an number to check whether its prime or not");
+        try{
+            s = br.readLine();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return s;
+    }
     public Client(String address, int port){
         try{
             socket = new Socket(address, port);
@@ -24,24 +77,5 @@ import java.io.*;
             e.printStackTrace();
         }
         
-        String s = "";
-        while(!s.equals("stop")){
-            try{
-                s = br.readLine();
-                ot.println(s);
-                ot.flush();
-                String response = serverReader.readLine();
-                System.out.println("Server says: " + response);
-            } catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-        try{
-            br.close();
-            ot.close();
-            socket.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
     }
  }
--- DCN P/Networking Task 1/Custom/Server.java
+++ DCN P/Networking Task 1/Custom/Server.java
@@ -3,13 +3,69 @@ import java.net.*;
 
 public class Server{
     public static void main(String args[]){
-        Server server = new Server(Integer.parseInt(args[0]));
+        try{
+            Server server = new Server(Integer.parseInt("12000"));
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            ot = new PrintWriter(socket.getOutputStream());
+            
+            while(true){
+                String s = server.read();
+                if(s==null||s.equals("stop"))
+                    break;
+                String res = server.find(s);
+
+                server.write(res);
+            }
+            server.close();
+            ot.close();
+            br.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
     static Socket socket;
     static ServerSocket server;
     static BufferedReader br;
     static PrintWriter ot;
 
+    static String find(String input){
+        String ans;
+        try{
+            int n = Integer.parseInt(input);
+            if(isPrime(n)){
+                ans = n + " is a prime number";
+            } else{
+                ans = n + " is a composite (non-prime) number";
+            }
+        } catch (Exception e){
+            ans = "Enter a valid number";
+        }
+        return ans;
+    }
+    static void write(String ans){
+        ot.println(ans);
+        ot.flush();
+    }
+    static String read(){
+        String input=new String();
+        try{
+            input = br.readLine();
+            System.out.println("Client Entered: " + input);
+            return input;
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }       
+        return input;
+    }
+    static void close(){
+        try{
+            System.out.println("Closing the connection");
+            socket.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     public Server(int port){
         try{
 
@@ -19,41 +75,9 @@ public class Server{
             System.out.println("Waiting for a client to connect....");
             socket = server.accept();
             System.out.println("Client accepted");
-
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ot = new PrintWriter(socket.getOutputStream());
-
-            String input = "";
-
-            while (input != null || !input.equals("stop")){
-                try{
-                    input = br.readLine();
-                    System.out.println("Client Entered: " + input);
-                    String ans = "";
-                    try{
-                        int n = Integer.parseInt(input);
-                        if(isPrime(n)){
-                            ans = n + " is a prime number";
-                        } else{
-                            ans = n + " is a composite (non-prime) number";
-                        }
-                    } catch (Exception e){
-                        ans = "Enter a valid number";
-                        // e.printStackTrace();
-                    }
-                    ot.println(ans);
-                    ot.flush();
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Closing the connection");
-            socket.close();
-            br.close();
         } catch (Exception e){
             e.printStackTrace();
         }
-
     }
     static boolean isPrime(int n){
         for(int i=2;i*i<=n;i++){
